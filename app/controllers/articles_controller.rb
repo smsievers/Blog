c79410f36dc1103c4684a0104135bb51873ee1c8
smsievers <seth_sievers@yahoo.com>
2020-08-17 20:22:30 -0500

    Added comments, still missing ability to remove comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,13 +19,19 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.new(params[:id])
+        @article = Article.find(params[:id])
 
         if @article.update(article_params)
             redirect_to @article
         else
             render 'edit'
         end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 
     def index
